5c15a45640ea7a2cc56c5d497284ad0a38dc079b
Darya Zakurdaeva <zakurdaevadsh@gmail.com>
2021-12-08 22:35:20 +0300

    bjs-diplom

--- public/homePage.js
+++ public/homePage.js
@@ -7,3 +7,18 @@ logoutButton.action = () => {
         } 
     }) 
 }
+
+const ratesBoard = new RatesBoard();
+
+function updateStocks() {
+    ApiConnector.getStocks((res) => {
+      if (res.success === true) {
+        ratesBoard.clearTable()
+        ratesBoard.fillTable(res.data)
+        return
+      }
+      console.log(res.error)
+    })
+  }
+  updateStocks();
+  setInterval(updateSocks, 1000 * 60);
--- public/loginPage.js
+++ public/loginPage.js
@@ -15,12 +15,4 @@ userForm.registerFormCallback = (data => {
   }
  })
 })
-
-const logoutButton = new LogoutButton()
-
-logoutButton.action = (data => {
-    ApiConnector.logout(data, (res) => {
-        console.log(res)
-    })
-   })
    
